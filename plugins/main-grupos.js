6b581dcf74ab94a46547689b293fd5973b21c8b6
Inemagu <inemagu28@gmail.com>
2024-11-20 17:33:13 -0400

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://telegra.ph/file/03d1e7fc24e1a72c60714.jpg`)).buffer()
+let img = await (await fetch(`https://d.uguu.se/dRTMOLYM.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `𝘛𝘦 𝘪𝘯𝘷𝘪𝘵𝘢𝘮𝘰𝘴 𝘢 𝘯𝘶𝘦𝘴𝘵𝘳𝘰𝘴 𝘨𝘳𝘶𝘱𝘰𝘴 𝘰𝘧𝘪𝘤𝘪𝘢𝘭𝘦𝘴. ⭐
-> 𝐒𝐈𝐒𝐊𝐄𝐃 𝐁𝐎𝐓 - 𝐌𝐃  
+let txt = `𝘛𝘦 𝘪𝘯𝘷𝘪𝘵𝘢𝘮𝘰𝘴 𝘢 𝘯𝘶𝘦𝘴𝘵𝘳𝘰𝘴 𝘨𝘳𝘶𝘱𝘰𝘴 𝘰𝘧𝘪𝘤𝘪𝘢𝘭𝘦𝘴. 🌸
+> 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩  
 
 *🎌* ${group}
 
